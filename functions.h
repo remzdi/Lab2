38ea64860724fc1ebfe3f3e21fd89be13e473e8b
remzdi <116484641+remzdi@users.noreply.github.com>
2023-06-22 14:19:16 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <fstream>
 #include <string>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 
-bool isValidPhoneNumber(const std::string& phoneNumber);
-void addCallInfo(ofstream& file);
-void createOutputFiles();
-void displayFileContents(const std::string& filename);
-bool addInformation(std::ofstream& inputFile);
+bool calculate_minutes (char* startTime, char* endTime);
+bool calculate_time(char* startTime, char* endTime);
+bool checkTimeFormat(char *input);
+void add_conversation(string filename);
+void read_info_from_file(string filename);
+void read_output_file(const string& filename);
