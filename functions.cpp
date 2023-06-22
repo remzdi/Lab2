d28cb91343f5d9aaf9256b419059e0ae3cfa8322
remzdi <116484641+remzdi@users.noreply.github.com>
2023-06-22 14:21:14 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,128 +1,234 @@
-#include "functions.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <cstring>
 
-bool isValidPhoneNumber(const std::string& phoneNumber) {
-    if (phoneNumber.length() != 5) {
+using namespace std;
+
+struct Conversation
+{
+    char phone_number[5];
+    char start_time[6];
+    char end_time[6];
+};
+
+bool calculate_minutes (char* startTime, char* endTime){
+
+    char minutes_start[3];
+    char minutes_end[3];
+    minutes_start[0] = startTime[3];
+    minutes_start[1] = startTime[4];
+    minutes_start[2] = '\0';
+
+    minutes_end[0] = endTime[3];
+    minutes_end[1] = endTime[4];
+    minutes_end[2] = '\0';
+
+    int startMinute = atoi(minutes_start);
+    int endMinute = atoi(minutes_end);
+
+    if (startMinute > endMinute) {
+        return true;
+    }
+    else if (startMinute < endMinute) {
         return false;
     }
+    else {
+        return true;
+    }
 
-    for (char c : phoneNumber) {
-        if (!isdigit(c)) {
+}
+
+bool calculate_time(char* startTime, char* endTime){
+    int checktime1 = 5;
+    int checktime2 = 6;
+    char hours_start[3];
+    char hours_end[3];
+
+    hours_start[0] = startTime[0];
+    hours_start[1] = startTime[1];
+    hours_start[2] = '\0';
+
+    hours_end[0] = endTime[0];
+    hours_end[1] = endTime[1];
+    hours_end[2] = '\0';
+
+    int startHour = atoi(hours_start);
+    int endHour = atoi(hours_end);
+
+    int time_to_check_start;
+    int time_to_check_end;
+
+    if ((startHour >= 6 && startHour < 20) && (endHour >= 6 && endHour < 20)) return true;
+    else if ((startHour >= 20 && endHour >= 20) || ((startHour < 6 && endHour < 6)) ||
+             (startHour >= 20 && endHour < 6)) return false;
+
+    else {
+
+        if (startHour >= 6 && endHour >= 20){
+            time_to_check_start = abs(checktime1 - startHour);
+            time_to_check_end = abs(checktime1 - endHour);
+        }
+        else if (startHour >= 6 && endHour < 6){
+            time_to_check_start = abs(checktime1 - startHour);
+            time_to_check_end = endHour + 4 ;
+        }
+        else if (startHour >= 20 && endHour >= 6){
+            time_to_check_end = (24 - startHour) + 6;
+            time_to_check_start = abs(checktime2 - endHour);
+
+        }
+        else if (startHour < 6 && endHour >= 6){
+            time_to_check_end = abs(checktime2 - startHour);
+            time_to_check_start = abs(checktime2 - endHour);
+
+        }
+
+
+        if (time_to_check_start > time_to_check_end)
+            return true;
+
+        else if (time_to_check_start < time_to_check_end)
             return false;
-        }
+        else return (calculate_minutes(startTime, endTime));
     }
-
-    return true;
 }
 
-void addCallInfo(ofstream& file) {
-    string phoneNumber, startTime, endTime;
-    do {
-        std::cout << "Enter the phone number (5 digits): ";
-        std::cin >> phoneNumber;
-        cout << endl;
-
-        if (!isValidPhoneNumber(phoneNumber)) {
-            std::cout << "Invalid phone number. Please enter 5 digits.\n";
-        }
-    } while (!isValidPhoneNumber(phoneNumber));
-
-    cout << endl;
-    cout << "Enter the start time of the call: ";
-    cin >> startTime;
-    cout << endl;
-    cout << "Enter the end time of the call: ";
-    cin >> endTime;
-    cout << endl;
-    file << phoneNumber << ", " << startTime << ", " << endTime << "\n";
-}
-
-void createOutputFiles() {
-    ifstream inputFile("input.txt");
-    ofstream outputDay("output_day.txt");
-    ofstream outputNight("output_night.txt");
-
-    if (!inputFile) {
-        cerr << "Failed to open input.txt\n";
-        cout << endl;
-        return;
-    }
-
-    if (!outputDay) {
-        cerr << "Failed to create output_day.txt\n";
-        cout << endl;
-        return;
-    }
-
-    if (!outputNight) {
-        cerr << "Failed to create output_night.txt\n";
-        cout << endl;
-        return;
-    }
-
-    string line;
-    while (getline(inputFile, line)) {
-        string startTime = line.substr(line.find(",") + 1);
-        int hour = stoi(startTime.substr(0, startTime.find(":")));
-
-        if (hour >= 6 && hour < 20) {
-            outputDay << line << "\n";
-        } else {
-            outputNight << line << "\n";
-        }
-    }
-
-    cout << "Output files created successfully.\n";
-    cout << endl;
-
-    inputFile.close();
-    outputDay.close();
-    outputNight.close();
-}
-
-void displayFileContents(const string& filename) {
-    ifstream file(filename);
-
-    if (!file) {
-        cerr << "Failed to open " << filename << "\n";
-        return;
-    }
-
-    cout << "Contents of " << filename << ":\n";
-    string line;
-    while (getline(file, line)) {
-        cout << line << "\n";
-    }
-
-    file.close();
-}
-
-bool addInformation(std::ofstream& inputFile) {
-    bool addInfo = true;
-    while (addInfo) {
-        int choice;
-        cout << "Do you want to add information? (1-yes, 2-no)";
-        cout << endl;
-        char er, ch;
-        do {
-            er = 0;
-            cout << "Enter the number: ";
-            scanf_s("%d%c", &choice, &ch);
-            cout << endl;
-            if ((ch != '\n') || (choice < 1) || (choice > 2)) {
-                er = 1;
-                fflush(stdin);
-                printf("Incorrect input choice \n");
+bool checkTimeFormat(char *input)
+{
+    // Check if input is of length 5 and the third character is a colon
+    if (strlen(input) == 5 && input[2] == ':')
+    {
+        // Check if the first two characters are digits between 0 and 2
+        if (input[0] >= '0' && input[0] <= '2' && isdigit(input[1]))
+        {
+            // Check if the last two characters are digits between 0 and 5
+            if (input[3] >= '0' && input[3] <= '5' && isdigit(input[4]))
+            {
+                // Check if the time is within the valid range
+                if (input[0] == '2')
+                {
+                    // If the first digit is 2, the second digit cannot be greater than 3
+                    if (input[1] <= '3')
+                    {
+                        return true; // The input is in the correct format and range
+                    }
+                }
+                else
+                {
+                    // If the first digit is 0 or 1, the second digit can be any digit
+                    return true; // The input is in the correct format and range
+                }
             }
-        } while (er);
-
-        if (choice == 1) {
-            addCallInfo(inputFile);
-        } else if (choice == 2) {
-            addInfo = false;
-        } else {
-            cout << "Invalid choice. Please enter 1 or 2.\n";
-            cout << endl;
         }
     }
-    return true; // Indicate that information was added
+    return false; // The input is not in the correct format or range
 }
+
+void add_conversation(string filename)
+{
+    Conversation conver;
+
+    do {
+
+        cout << "\nEnter phone number: ";
+        cin >> conver.phone_number;
+        cout << endl;
+        if (strlen(conver.phone_number) > 5){
+            cout << "Too long phone number. Try again." << endl;
+        }
+    }while((strlen(conver.phone_number) > 5));
+
+    cin.ignore();
+    do
+    {
+        cout << "Enter start time (YY:XX): ";
+        cin >> conver.start_time;
+        if (!checkTimeFormat(conver.start_time))
+        {
+            cout << "Wrong input format. Try again." << endl;
+        }
+    } while (!checkTimeFormat(conver.start_time));
+
+    do
+    {
+        cout << "Enter end time (YY:XX): ";
+        cin >> conver.end_time;
+        if (!checkTimeFormat(conver.end_time))
+        {
+            cout << "Wrong input format. Try again." << endl;
+        }
+    } while (!checkTimeFormat(conver.end_time));
+    cin.ignore();
+
+
+    ofstream outfile(filename, ios::binary | ios::app);
+    if (outfile.is_open())
+    {
+        outfile.write((char *)&conver, sizeof(Conversation));
+        outfile.close();
+        cout << "Conversation added successfully.\n";
+    }
+    else
+    {
+        cout << "Error: Unable to open file.\n";
+    }
+}
+
+void read_info_from_file(string filename)
+{
+
+    ifstream file(filename, ios::binary);
+    ofstream fout_day("daytime_calls.bin", ios::binary);
+    ofstream fout_night("nighttime_calls.bin", ios::binary);
+    if (!file)
+    {
+        cout << "Error: could not open file " << filename << endl;
+        return;
+    }
+    else{
+        cout << "File was opened for reading" << endl;
+    }
+
+    Conversation information;
+    while (file.read((char *)&information, sizeof(Conversation)))
+    {
+
+        if (calculate_time(information.start_time, information.end_time)){
+            fout_day.write((char*)&information, sizeof(Conversation));
+
+        }
+        else {
+            fout_night.write((char*)&information, sizeof(Conversation));
+        }
+
+
+    }
+    cout << "The information was sorted" << endl;
+    file.close();
+    fout_day.close();
+    fout_night.close();
+}
+
+void read_output_file(const string& filename)
+
+{
+    ifstream infile(filename, ios::binary | ios::in);
+
+    if (!infile)
+    {
+        cerr << "Error: could not open file " << filename << endl;
+        return;
+    }
+    Conversation conv;
+    while (infile.read(reinterpret_cast<char*>(&conv), sizeof(Conversation)))
+    {
+        cout << "Phone number: " << string(conv.phone_number).substr(0, 5) << endl;
+        cout << "Start time: " << conv.start_time << endl;
+        cout << "End time: " << conv.end_time << endl;
+    }
+
+    infile.close();
+}
+
+
